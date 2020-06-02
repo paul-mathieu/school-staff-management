e8e800bd67bac8213ac46a81e9c9dc3ef7cac8c5
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-06-02 21:25:13 +0200

    final changes on message tab

--- tab/page_2_new_message.php
+++ tab/page_2_new_message.php
@@ -1,52 +1,72 @@
 <?php
-$sql = "SELECT nom, prenom, login FROM molierej.utilisateur
-	INNER JOIN molierej.statut ON molierej.statut.statut = 'Enseignant'
-        AND molierej.statut.id_statut = molierej.utilisateur.statut_id_statut
-	ORDER BY nom";
-$result_name = mysql_query($sql) or die('Erreur SQL ! '.$sql.'<br>'.mysql_error());
-
 if (isset($_GET["id_request"])) {
 	$id_requete = $_GET["id_request"];
 } else {
 	$id_requete = 0;
 }
 
-?>
+if (!(isset($_GET["second"]))) {
+	echo"
+	<div id=bot-right class='container-message'>
 
+    <div class='title'>Nouveau message</div></br>
+
+    <form action='?page=2&id_request=".$id_requete."&second=true' method='post' class='form-container-messagerie'>
+
+        <div>Profession du destinataire :
+            <select name='prof'>
+				<option>Enseignant</option>
+				<option>Etudiant</option>
+				<option>Expert</option>
+				<option>Comptable</option>
+            </select>
+        </div></br>
+
+      <input type='submit' value='Rédiger le message'>
+
+    </form>
+</div>";
+} else {
+$profession = $_POST['prof'];
+$sql = "SELECT nom, prenom, login FROM molierej.utilisateur
+	INNER JOIN molierej.statut ON molierej.statut.statut = '".$profession."'
+    AND molierej.statut.id_statut = molierej.utilisateur.statut_id_statut
+	ORDER BY nom";
+$result_name = mysql_query($sql) or die('Erreur SQL ! '.$sql.'<br>'.mysql_error());
+
+echo"
 <div id=bot-right class='container-message'>
 
     <div class='title'>Nouveau message</div>
 
-    <form action=<?php echo "'?page=2&id_request=".$id_requete."&new_message=true'"?> method='post' class='form-container-messagerie'>
+    <form action='?page=2&id_request=".$id_requete."&new_message=true' method='post' class='form-container-messagerie'>
 
-        <div>Nom de l'enseignant destinataire :
-            <select name="login">
-                <?php
+        <div>Destinataire (".$profession.") :
+            <select name='login'>";
                 if (mysql_num_rows($result_name) > 0){
                     while ($row = mysql_fetch_array($result_name, MYSQL_NUM)) {
                         echo '		<option value="' . $row[2] . '">' . $row[1] . ' ' . $row[0] . '</option>';
                     }
-                }
-                ?>
+                } echo"
             </select>
         </div>
 
-        <textarea id='content' class="big-zone" name='content' rows='3' cols='50' placeholder='Écrivez ici le contenu de votre message. Il sera visible par toutes les personnes concernées'></textarea>
+        <textarea id='content' class='big-zone' name='content' rows='3' cols='50' placeholder='Écrivez ici le contenu de votre message. Il sera visible par toutes les personnes concernées'></textarea>
 
-<!--      <br>Ajouter une pièce jointe :-->
-       <input type="file" name="fileToUpload" id="fileToUpload" title="Ajouter une pièce jointe">
+       <input type='file' name='fileToUpload' id='fileToUpload' title='Ajouter une pièce jointe'>
 
       <input type='submit' value='Envoyer le message'>
 
     </form>
-</div>
+</div>";
 
-<?php 
+}
+
 if (isset($_GET["new_message"])) {
 	if (empty($_POST['fileToUpload'])){
 		$piece_jointe = 'NULL';
 	} else {
-		$piece_jointe = $_POST['fileToUpload'];
+		$piece_jointe = 'NULL';
 	}
 	$sql = "insert into message (utilisateur_login, id_requete, message, piece_jointe) values ('".
 	$login."',".$id_requete.",'".$_POST['content']."',".$piece_jointe.")";
